49a5e9cf8554a18b929470f3f51fecc454743d86
so34so <76669772+so34so@users.noreply.github.com>
2023-01-30 20:43:56 +0900

    [Bronze V] Title: A/B, Time: 204 ms, Memory: 17828 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		double a = sc.nextInt();
+		double b = sc.nextInt();
+		double result = a / b;
+		
+		System.out.println(result);
+	}
+
+}
