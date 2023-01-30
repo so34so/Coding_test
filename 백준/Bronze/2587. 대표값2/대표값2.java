f3ce717df3344905eb761d6399ae8d57e95d520b
so34so <76669772+so34so@users.noreply.github.com>
2023-01-30 23:08:28 +0900

    [Bronze II] Title: 대표값2, Time: 200 ms, Memory: 17684 KB -BaekjoonHub

---
+++ 백준/Bronze/2587. 대표값2/대표값2.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int[] arr = new int[5];
+		int sum = 0;
+		for(int i=0;i<5;i++) {
+			arr[i] = sc.nextInt();
+			sum += arr[i];
+		}
+		int N = sum/5;
+		Arrays.sort(arr);
+		System.out.println(N);
+		System.out.println(arr[2]);
+	}	
+}
