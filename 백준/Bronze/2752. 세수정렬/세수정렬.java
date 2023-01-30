2bd7d29fda8e252253b2f16ef36cc66887446ddb
so34so <76669772+so34so@users.noreply.github.com>
2023-01-30 20:52:55 +0900

    [Bronze IV] Title: 세수정렬, Time: 144 ms, Memory: 15904 KB -BaekjoonHub

---
+++ 백준/Bronze/2752. 세수정렬/세수정렬.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
+		StringTokenizer st = new StringTokenizer(str," ");
+		int[] arr = new int[3];
+		for(int i=0;i<3;i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr);
+		for(int i=0; i<3;i++) {
+			System.out.print(arr[i] + " ");
+		}
+	}
+}
