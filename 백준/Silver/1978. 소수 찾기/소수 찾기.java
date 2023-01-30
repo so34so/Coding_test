cd5ecba8b7f6f0f373b4a9df9fe1ee8f54b9ecbd
so34so <76669772+so34so@users.noreply.github.com>
2023-01-30 20:46:14 +0900

    [Silver V] Title: 소수 찾기, Time: 216 ms, Memory: 17732 KB -BaekjoonHub

---
+++ 백준/Silver/1978. 소수 찾기/소수 찾기.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int count = 0;
+		for(int i=0; i<N;i ++) {
+			boolean in = true;
+			int num = sc.nextInt();
+			if(num==1) {
+				continue;
+			}
+			for(int j=2;j<num;j++) {
+				if(num%j==0) {
+					in = false;
+					break;
+				}
+			}
+			if(in) {
+				count++;
+			}
+		}
+		System.out.println(count);
+	}
+}
