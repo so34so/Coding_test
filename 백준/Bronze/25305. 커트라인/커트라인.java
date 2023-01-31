b306dd4a5bf6032047afbfd9c8d0b7bc14291403
so34so <76669772+so34so@users.noreply.github.com>
2023-01-31 20:05:45 +0900

    [Bronze II] Title: 커트라인, Time: 284 ms, Memory: 20676 KB -BaekjoonHub

---
+++ 백준/Bronze/25305. 커트라인/커트라인.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int k = sc.nextInt();
+		Integer[] arr = new Integer[N];
+		
+		for(int i=0;i<N;i++) {
+			arr[i] = sc.nextInt();
+		}
+		Arrays.sort(arr, Collections.reverseOrder());
+		System.out.println(arr[k-1]);
+	}	
+}
