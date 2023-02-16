6d415489d423230d4878f91dffb41e3f556225bc
so34so <76669772+so34so@users.noreply.github.com>
2023-02-16 21:51:26 +0900

    [Silver I] Title: 배열 돌리기 1, Time: 1616 ms, Memory: 37580 KB -BaekjoonHub

---
+++ 백준/Silver/16926. 배열 돌리기 1/배열 돌리기 1.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	private static int[] dx = {0,1,0,-1};
+	private static int[] dy = {1,0,-1,0};
+	static StringTokenizer st; 
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int R = Integer.parseInt(st.nextToken());
+		int[][] arr = new int[N][M];
+		
+		for(int i=0;i<N;i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j=0;j<M;j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		int group = Math.min(N, M)/2;
+		
+		for(int i=0;i<R;i++) {
+			for(int j=0;j<group;j++) {
+				int x=j;
+				int y=j;
+				int value = arr[x][y];
+				int idx=0;
+				
+				while(idx<4) {
+					int nx = x+dx[idx];
+					int ny = y+dy[idx];
+					if(nx>=j && ny>=j && nx<N-j && ny<M-j) {
+						arr[x][y] = arr[nx][ny];
+						x=nx;
+						y=ny;
+					}else {
+						idx++;
+					}
+				}
+				arr[j+1][j] = value;
+			}
+		}
+		
+		for(int i=0;i<N;i++) {
+			for(int j=0;j<M;j++) {
+				System.out.print(arr[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+}
