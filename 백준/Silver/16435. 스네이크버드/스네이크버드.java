0d0ed346a2030473df979e7d2fdfc4f51b5598e7
so34so <76669772+so34so@users.noreply.github.com>
2023-02-17 21:53:57 +0900

    [Silver V] Title: 스네이크버드, Time: 132 ms, Memory: 14392 KB -BaekjoonHub

---
+++ 백준/Silver/16435. 스네이크버드/스네이크버드.java
@@ -0,0 +1,37 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main  {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int L = Integer.parseInt(st.nextToken());
+		int[] arr = new int[N];
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i=0;i<N;i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr);
+		int cnt =0;
+		while(true) {
+			if(cnt==N) {
+				break;
+			}
+			if(L >= arr[cnt]) {
+				L++;
+			}
+			cnt++;
+		}
+		System.out.println(L);
+	}
+}
