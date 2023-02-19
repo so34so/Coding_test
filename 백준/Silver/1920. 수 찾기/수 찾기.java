42ce8037d82cf74943c0af12ed0341086da02a58
so34so <76669772+so34so@users.noreply.github.com>
2023-02-19 10:11:27 +0900

    [Silver IV] Title: 수 찾기, Time: 1224 ms, Memory: 51668 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int[] arr = new int[N];
+		for(int i=0;i<N;i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr);
+		int M = Integer.parseInt(br.readLine());
+		st = new StringTokenizer(br.readLine());
+		for(int i=0;i<M;i++) {
+			int answer = 0;
+			int start = 0;
+			int end = N-1;
+			int x = Integer.parseInt(st.nextToken());
+			while(start<=end) {
+				int mid = (start+end)/2;
+				if(arr[mid]==x) {
+					answer = 1;
+					break;
+				}
+				if(arr[mid]>x) end=mid-1;
+				else start = mid+1;
+			}
+			System.out.println(answer);
+		}
+	}
+}
