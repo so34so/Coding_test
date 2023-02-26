fc9f1d1b2538f63d2fca3b1656c3e19992e0e6ba
so34so <76669772+so34so@users.noreply.github.com>
2023-02-26 23:40:41 +0900

    [Silver V] Title: 막대기, Time: 124 ms, Memory: 14172 KB -BaekjoonHub

---
+++ 백준/Silver/1094. 막대기/막대기.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int X = Integer.parseInt(br.readLine());
+		int count = 0;
+		int stick = 64;
+		while(X>0) {
+			if(stick>X)stick /= 2;
+			else {
+				X -=stick;
+				count++;
+			}
+		}
+		System.out.println(count);
+	}
+}
