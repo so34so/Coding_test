3810e7904cd75fed05b18a4a5806aaa954b7d8fd
so34so <76669772+so34so@users.noreply.github.com>
2023-03-05 00:02:51 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 1456 ms, Memory: 58968 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,20 +1,26 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	
-	public static void main(String[] args){
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		int M = sc.nextInt();
-		int[] arr = new int[N+1];
-		arr[0] = 0;
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int[] num = new int[N+1];
+		num[0] = 0;
+		st = new StringTokenizer(br.readLine());
 		for(int i=1;i<=N;i++) {
-			arr[i] = arr[i-1] + sc.nextInt();
+			num[i] = num[i-1] + Integer.parseInt(st.nextToken());
 		}
 		for(int i=0;i<M;i++) {
-			int a = sc.nextInt();
-			int b = sc.nextInt();
-			System.out.println(arr[b]-arr[a-1]);
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			System.out.println(num[b]-num[a-1]);
 		}
+		
 	}
 }
