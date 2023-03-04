da67e735c4689e4f98f9e324b9da801d0f67457e
so34so <76669772+so34so@users.noreply.github.com>
2023-03-04 23:00:47 +0900

    [Silver III] Title: N과 M (5), Time: 360 ms, Memory: 28308 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int N,M,temp[],num[];
+	static boolean check[];
+	static StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		num = new int[N];
+		check = new boolean[N];
+		st = new StringTokenizer(br.readLine());
+		for(int i=0;i<N;i++) {
+			num[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(num);
+		
+		temp = new int[M];
+		sol(0);
+		System.out.println(sb);
+	}
+	private static void sol(int n) {
+		if(n==M) {
+			for(int i=0;i<M;i++) {
+				sb.append(temp[i]).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		for(int i=0;i<N;i++) {
+			if(check[i]) continue;
+			temp[n] = num[i];
+			check[i] = true;
+			sol(n+1);
+			check[i] = false;
+		}
+	}
+}
