8322abf0cdfa5f562a5f03f99204590508207a6f
so34so <76669772+so34so@users.noreply.github.com>
2023-07-19 22:15:18 +0900

    [unrated] Title: n 번째 원소부터, Time: 0.02 ms, Memory: 72.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181892. n 번째 원소부터/n 번째 원소부터.java
@@ -0,0 +1,7 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] list, int n) {
+        int[] num_arr = Arrays.copyOfRange(list, n-1, list.length);
+        return num_arr;
+    }
+}
