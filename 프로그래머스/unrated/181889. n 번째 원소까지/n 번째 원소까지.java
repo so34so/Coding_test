5aa8e66427d4cbee352d6490b538e3038933c912
so34so <76669772+so34so@users.noreply.github.com>
2023-07-25 21:06:52 +0900

    [unrated] Title: n 번째 원소까지, Time: 0.03 ms, Memory: 75.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181889. n 번째 원소까지/n 번째 원소까지.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int[] solution(int[] list, int n) {
+        int[] answer = new int[n];
+        for(int i=0;i<n;i++){
+            answer[i] =list[i]; 
+        }
+        return answer;
+    }
+}
