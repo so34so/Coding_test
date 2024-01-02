a9ec449716f0c968bbb2db537e706488abe0f22e
so34so <76669772+so34so@users.noreply.github.com>
2024-01-02 15:00:56 +0900

    [Silver IV] Title: 수 찾기, Time: 648 ms, Memory: 44336 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -5,32 +5,36 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int[] arr = new int[N];
-		for(int i=0;i<N;i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
-		Arrays.sort(arr);
-		int M = Integer.parseInt(br.readLine());
-		st = new StringTokenizer(br.readLine());
-		for(int i=0;i<M;i++) {
-			int answer = 0;
-			int start = 0;
-			int end = N-1;
-			int x = Integer.parseInt(st.nextToken());
-			while(start<=end) {
-				int mid = (start+end)/2;
-				if(arr[mid]==x) {
-					answer = 1;
-					break;
-				}
-				if(arr[mid]>x) end=mid-1;
-				else start = mid+1;
-			}
-			System.out.println(answer);
-		}
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[N];
+        for(int i=0;i<N;i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for(int i=0;i<M;i++){
+            int answer = 0;
+            int start = 0;
+            int end = N-1;
+            int x = Integer.parseInt(st.nextToken());
+            while(start<=end){
+                int mid = (start+end)/2;
+                if(arr[mid]==x){
+                    answer = 1;
+                    break;
+                }else if(arr[mid]>=x){
+                    end = mid-1;
+                }else{
+                    start = mid+1;
+                }
+            }
+            sb.append(answer).append("\n");
+        }
+        System.out.print(sb);
+    }
 }
