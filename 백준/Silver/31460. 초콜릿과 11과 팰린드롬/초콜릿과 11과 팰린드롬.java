077550b3d8847b4fc1c60cb67f162a44b2ef8910
so34so <76669772+so34so@users.noreply.github.com>
2024-03-07 21:17:42 +0900

    [Silver IV] Title: 초콜릿과 11과 팰린드롬, Time: 264 ms, Memory: 22116 KB -BaekjoonHub

---
+++ 백준/Silver/31460. 초콜릿과 11과 팰린드롬/초콜릿과 11과 팰린드롬.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+  public static void main(String[] args) throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringBuilder sb = new StringBuilder();
+    int T = Integer.parseInt(br.readLine());
+
+    for(int i=0;i<T;i++){
+      int num = Integer.parseInt(br.readLine());
+
+      if(num==1){
+        sb.append("0");
+      }else if(num % 2 ==0){
+        for(int j=1;j<=num;j++){
+          if(j==1 || j == num){
+            sb.append("1");
+          }else{
+            sb.append("2");
+          }
+        }
+      }else if(num % 2 !=0){
+        for(int j=1;j<=num;j++){
+          if(j==1 || j == num){
+            sb.append("1");
+          }else{
+            sb.append("2");
+          }
+        }
+      }
+      sb.append("\n");
+    }
+    System.out.println(sb);
+  }
+
+
+}
